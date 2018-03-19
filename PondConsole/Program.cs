92fb7666fa8a23cd6163804aaa470a5583ee640d
StragaSevera <ought@yandex.ru>
2018-03-19 18:15:29 +0300

    Add PondConsole for testing PondLibrary

---
+++ PondConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using PondLibrary;
+
+namespace PondConsole
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var pond = new Pond(5);
+
+            foreach(int item in pond.FoodEntities)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
