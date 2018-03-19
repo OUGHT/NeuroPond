77e775c4dd88f2bab84cebb93469b9a0068d5665
StragaSevera <ought@yandex.ru>
2018-03-19 18:09:54 +0300

    Implement basic PondLibrary

--- PondLibrary/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PondLibrary
-{
-    public class Class1
-    {
-    }
-}
---
+++ PondLibrary/Pond.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PondLibrary
+{
+    public class Pond
+    {
+        private const double XMin = 0.0;
+        private const double YMin = 0.0;
+        private const double XMax = 1000.0;
+        private const double YMax = 1000.0;
+
+        public Random Rand { get; set; } = new Random();
+
+        private List<int> foodEntities;
+        public List<int> FoodEntities => foodEntities;
+
+        public Pond()
+        {
+        }
+
+        public Pond(int foodAmount)
+        {
+            foodEntities = Enumerable.Range(1, foodAmount)
+                                     .Select(x => Rand.Next()).ToList();
+        }
+
+
+    }
+}
